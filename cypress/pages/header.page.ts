c8db1066f353cfcea204f6ed92b6c81e1ca58814
Vinay M S <m.vinay@powerschool.com>
2024-08-28 22:52:13 +0530

    added search page tests

--- cypress/pages/header.page.ts
+++ cypress/pages/header.page.ts
@@ -1,8 +1,13 @@
 const SIGN_IN_BTN = '[title="My Account"]';
+const SEARCH_BAR = '.hidden-mobile .st-search-input';
 
 class HeaderPage {
     public get signInButton(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(SIGN_IN_BTN);
+    }
+
+    public get searchBar(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(SEARCH_BAR);
     }
 }
 
